a3e919e505b73e482ddf958fb29ee3412558cf93
yesenia <yeseniab743@gmail.com>
2023-11-20 19:55:35 -0500

    added more questions with returns

--- questions.js
+++ questions.js
@@ -17,5 +17,21 @@ const questions = [
 },
 
 {
-    
-}
+   type: "input"
+   name: "shapeColor",
+   message: "Enter the shape color keyword",
+   when: (answers)=> {
+    if(answers.shapecolorchoice === 'color keyword') {
+        return true;
+    }
+    return false;
+   } 
+},
+validate: (answer) => {
+    let answerLowercase = answer.toLowerCase();
+    for (var i = 0, len = colorkeywords.length; i < len; ++i){
+        if (answerLowercase.indexof(colorKeywords[i]) != -1){
+           return true;
+        }}
+        return console.log("\n please enter a valid color keyword")
+},
